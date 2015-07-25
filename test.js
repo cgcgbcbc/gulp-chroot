1e9a51731d63dbb334f6260a1cbd09351a1943a7
cgcgbcbc <cgcgbcbc@163.com>
2015-07-26 06:17:46 +0800

    update test

--- test.js
+++ test.js
@@ -14,12 +14,20 @@ after(function(done) {
   fs.rmdir('child', done);
 });
 
-it('should chroot', function() {
+it('should chroot and restore', function() {
   gulp.chroot('child', function(){
     gulp.task('test', function() {
       console.log('inside test task');
+      console.log(process.cwd());
       assert.equal(process.cwd(), path.join(__dirname, 'child'));
     });
   });
+  gulp.task('restore', function() {
+      console.log('inside test task');
+      console.log(process.cwd());
+      assert.equal(process.cwd(), path.join(__dirname));
+  });
+  gulp.start('test');
+  gulp.start('restore');
 });
 
